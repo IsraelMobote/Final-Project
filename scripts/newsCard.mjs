5893c73f071d017af8887eceab71879f23956eca
IsraelMobote <moboteisrael02@gmail.com>
2025-06-16 07:40:38 +0100

    edit

--- scripts/newsCard.mjs
+++ scripts/newsCard.mjs
@@ -1,11 +1,14 @@
 import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
 
+
 function getNewsCardTemplate(newsItem) {
-    return `<div class="news-card">
+
+
+    return `<a href="${newsItem.url}" target="_blank"><div class="news-card">
             <img src="${newsItem.image}" alt="image showing ${newsItem.title}">
             <h2>${newsItem.title}</h2>
             <p>${newsItem.description}</p>
-        </div>`;
+        </div></a>`;
 }
 
 const newsList = getLocalStorage("NewsDataList");
