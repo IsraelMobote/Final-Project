e219c7074d43e8a42b683750499d417212f55f79
IsraelMobote <moboteisrael02@gmail.com>
2025-06-18 13:03:54 +0100

    work on the design of the index page

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 
 import externalServices from "./externalServices.mjs";
 import { loadHeaderFooter } from "./HeaderAndFooter.mjs";
+import { countries } from "./newsList.mjs";
 
 const services = new externalServices();
 
@@ -12,3 +13,16 @@ const header = document.querySelector("#main-header");
 const footer = document.querySelector("#main-footer");
 
 loadHeaderFooter(header, footer);
+
+// code that inputs the countries as options for the select element in the home page form
+
+function addOptionsToSelectElement(element, optionList) {
+    optionList.forEach(optionText => {
+        const optionElement = document.createElement("option");
+        optionElement.textContent = optionText;
+        element.append(optionElement);
+    });
+}
+
+const selectElement = document.querySelector("#countrySelector");
+addOptionsToSelectElement(selectElement, countries);
